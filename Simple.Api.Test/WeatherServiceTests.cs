60faf6ceb91ad8f0966e645887ba487ea873e382
majeed <majeed.dl@gmail.com>
2023-10-18 11:08:45 +0300

    feat: create unit tests for simple api weather service

---
+++ Simple.Api.Test/WeatherServiceTests.cs
@@ -0,0 +1,32 @@
+
+
+using Simple.Api.Interfaces;
+using Simple.Api.Service;
+
+namespace Simple.Api.Test;
+
+public class WeatherServiceTests
+{
+
+
+    IWeatherService _weatherService;
+
+    public WeatherServiceTests()
+    {
+        _weatherService = new WeatherService();
+    }
+
+    [Fact]
+    public void GetWeatherForecasts_ReturnsWeatherForecasts()
+    {
+        // Arrange
+        var expected = 5;
+
+        // Act
+        var actual = _weatherService.GetWeatherForecasts();
+
+        // Assert
+        Assert.Equal(expected, actual.Count());
+    }
+
+}
